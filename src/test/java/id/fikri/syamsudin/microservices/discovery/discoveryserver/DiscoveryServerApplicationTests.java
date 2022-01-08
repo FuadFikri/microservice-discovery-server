246eb2e97aaf7c7e9122b1f6a91bf1c3f5a3d7e8
fikri <fuadfikri01@gmail.com>
2022-01-09 06:05:37 +0700

    test commit

--- src/test/java/id/fikri/syamsudin/microservices/discovery/discoveryserver/DiscoveryServerApplicationTests.java
+++ src/test/java/id/fikri/syamsudin/microservices/discovery/discoveryserver/DiscoveryServerApplicationTests.java
@@ -3,11 +3,11 @@ package id.fikri.syamsudin.microservices.discovery.discoveryserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DiscoveryServerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class DiscoveryServerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
